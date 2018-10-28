ce0a12086ba21559dd0c31e58cdbfa9d446df9dc
Ryan Stachura <stachurr@oregonstate.edu>
2018-10-27 18:48:17 -0700

    Just beginning, moving to PC

--- index.js
+++ index.js
@@ -2,6 +2,24 @@
  * Write your JS code in this file.  Don't forget to include your name and
  * @oregonstate.edu email address below.
  *
- * Name:
- * Email:
+ * Name:    Ryan Stachura
+ * Email:   stachurr@oregonstate.edu
  */
+
+/* modal backdrop id = modal-backdrop */
+/* modal id = sell-something-modal */
+
+/*==================== Event Listeners ====================*/
+document.getElementById("sell-something-button").addEventListener("click", toggle_modal);
+document.getElementById("modal-close").addEventListener("click", toggle_modal);
+document.getElementById("modal-cancel").addEventListener("click", toggle_modal);
+
+/*====================    Functions    ====================*/
+/* Part 2 - modal */
+function toggle_modal() {
+    /*UNFINISHED: clear any user information before displaying*/
+    document.getElementById("post-text-input").innerHTML = "";
+
+    document.getElementById("modal-backdrop").classList.toggle('hidden');
+    document.getElementById("sell-something-modal").classList.toggle('hidden');
+}
